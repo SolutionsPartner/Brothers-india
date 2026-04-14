d0de93d6d570aae3a6e0e0eead3ca6346b2abc2a
KashiPriya <priyakashi485@gmail.com>
2026-04-14 11:39:56 +0530

    chat box

--- script.js
+++ script.js
@@ -213,3 +213,14 @@ document.addEventListener('DOMContentLoaded', () => {
   initProgressBar();
   initNavbarScroll();
 });
+
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/69d76e0bacf4021c348424cf/1jlooveo0';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
